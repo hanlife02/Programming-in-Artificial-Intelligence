7f1825d83d0cd61e3971ee8542fad4bd2a00bfe3
Ethan <2300011177@stu.pku.edu.cn>
2025-09-15 19:50:18 +0800

    feat: 添加数据集下载和加载功能

--- hw1/test.py
+++ hw1/test.py
@@ -1,6 +1,8 @@
 import torch
 import torchvision
+import torchvision.transforms as transforms
 
+# data download and data loader
 batch_size = 4
 transform = torchvision.transforms.Compose([
     transforms.ToTensor(),
@@ -9,3 +11,4 @@ transform = torchvision.transforms.Compose([
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(
 trainset,batch_size=batch_size, shuffle=True, num_workers=2)
+
